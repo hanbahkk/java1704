4f47a49790f031ce0e424a0f04b3b5f134b0f5f6
hanbahkk <58yjhb86@gmail.com>
2022-09-29 17:26:08 +0900

    1

---
+++ java_2/src/java_2/java_2_1.java
@@ -0,0 +1,10 @@
+package java_2;
+
+public class java_2_1{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("americano shot 1, plus big very hot.") ;
+	}
+
+}
---
+++ java_2/src/java_2_2/java_2_3.java
@@ -0,0 +1,10 @@
+package java_2_2;
+
+public class java_2_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("black pinky sister") ;
+	}
+	
+}
